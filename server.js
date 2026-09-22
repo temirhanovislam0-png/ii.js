d1d724fce653b625cfb2eeb2288e4c8193e0db0f
Темирханов Ислам А <student314@ithub.local>
2026-09-22 11:48:55 +0300

    НОвый проект

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const http = require('http')
-const express = require('express');
-const { log } = require('console');
-const app = express ()
+const express = require('express'); // подключили библиотеку Express
+const app = express () // запустили Express 
 
-const PORT = 3000;
+const PORT = 3000; //обозначили порт для дальнейшего запуска
 
-app.get("/", (req , res)=> {
-    res.send("Главная страница")
+app.get("/", (req , res)=> { //создаем get запрос для получения главной страницы
+    res.send("Главная страница")//отправляем клиенту ответ respons
 })
-app.listen(PORT, ()=>{
+app.get("/about", (req , res)=> { //создаем get запрос для получения главной страницы
+    res.send("О нас")//отправляем клиенту ответ respons
+})
+app.get("/contact", (req , res)=> { //создаем get запрос для получения главной страницы
+    res.send("Наш номер: 89287654321")//отправляем клиенту ответ respons
+})
+app.get('/product/:id',(req,res)=>{
+    console.log(req.params);
+    res.send(`Товар с id: ${req.params.id}`)
+    
+})
+app.get('/phone/:number',(req , res)=>{
+    console.log(req.params);
+    res.send(`Ваш номер телефона: ${req.params.number}`)
+})
+app.listen(PORT, ()=>{ //настраиваем сервер на чем он будет запущен
     console.log(`Сервер успешно запушен по адресу: 127.0.0.1:${PORT}`);
     
 })
